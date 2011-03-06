f358ca4d5cd850f4caf739ede11b97bfd3d3006f
Kelley van Evert <kelley.vanevert@gmail.com>
2011-03-06 21:14:21 +0100

    Nu worden de lijnen aan de hand van vierhoekige path-segmenten opgeslagen. Nog steeds: lijnen=zwart; achtergrond=rotatie-kleur. Teken een stip met een klik, clear canvas met dubbelklik.

---
+++ CallByDoodle/src/studie/callbydoodle/Vec.java
@@ -0,0 +1,59 @@
+package studie.callbydoodle;
+
+// Very simple 2D vector class
+// Immutable
+public class Vec
+{
+	private float x, y;
+	
+	public Vec(float x, float y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Vec add(Vec other)
+	{
+		return new Vec(x + other.x, y + other.y);
+	}
+	
+	public Vec subtract(Vec other)
+	{
+		return new Vec(x - other.x, y - other.y);
+	}
+	
+	public Vec times(float c)
+	{
+		return new Vec(x*c, y*c);
+	}
+	
+	public float getLength()
+	{
+		return (float)Math.sqrt((double)x*x + y*y);
+	}
+	
+	public Vec setLength(float len)
+	{
+		return this.times(len/this.getLength());
+	}
+	
+	public Vec rotateLeft()
+	{
+		return new Vec(y, -x);
+	}
+	
+	public Vec rotateRight()
+	{
+		return new Vec(-y, x);
+	}
+	
+	public float getX()
+	{
+		return x;
+	}
+	
+	public float getY()
+	{
+		return y;
+	}
+}
