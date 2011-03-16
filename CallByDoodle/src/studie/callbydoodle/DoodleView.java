bffcd32d01ebf49397b738bfe193e0113bb25b3a
Roelf Leenders <roelf.leenders@gmail.com>
2011-03-16 17:47:14 +0100

    Headers toegevoegd met project informatie

--- CallByDoodle/src/studie/callbydoodle/DoodleView.java
+++ CallByDoodle/src/studie/callbydoodle/DoodleView.java
@@ -1,4 +1,25 @@
+/*=============================================================================
+ * CallByDoodle
+ * 
+ * CallByDoodle is an Android project created in the Spring of 2011
+ * for Research & Development 1, a course the authors follow at
+ * Radboud University, Nijmegen.
+ * 
+ * Authors:
+ * Roelf Leenders
+ * Kelley van Evert
+ * Jochem Kooistra
+ * 
+ * Supervisors:
+ * Erik Barendsen
+ * Sjaak Smetsers
+ * 
+ * Project started in February of 2011, scheduled for completion 
+ * in the summer of 2011.
+ ============================================================================*/
+
 package studie.callbydoodle;
+
 
 import java.util.ArrayList;
 
